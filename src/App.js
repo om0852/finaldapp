a5fc5457e5cb0bedbc74bb1da9bf789929efd4b1
om salunke <omsaunke343@gmail.com>
2023-08-29 00:53:28 +0530

    app

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import Auction from './static/Auction.json';
 import { useState } from 'react';
 // import { Component } from 'react';
 function App (){
-  const [myContract,setmyContract]=useState(null);
+//   const [myContract,setmyContract]=useState(null);
 const numberSet = async()=>{
-  const web3  = new Web3(window.ethereum);
-  console.log(web3)
-  const myContractq  =new web3.eth.Contract(Auction,"0xdf655F272f5Fa1A97808B3bAeF92b2923B7e9970")
-  setmyContract(myContractq);
-  console.log(myContract);
-  // const gas = await myContract.methods.setno(210).estimateGas();
-  // const post = await myContract.methods.setno(210).send({
-  //   from: "0x8eAc8b24ac81D0BA0dc21f33d14321b28Bd2b971",
-  //   gas,
-  // });
-}
-const  getdata = async()=>{  
-  const data = await myContract.methods.getno().call();
-  console.log(data.toString());
+//   const web3  = new Web3(window.ethereum);
+//   console.log(web3)
+//   const myContractq  =new web3.eth.Contract(Auction,"0xdf655F272f5Fa1A97808B3bAeF92b2923B7e9970")
+//   setmyContract(myContractq);
+//   console.log(myContract);
+//   // const gas = await myContract.methods.setno(210).estimateGas();
+//   // const post = await myContract.methods.setno(210).send({
+//   //   from: "0x8eAc8b24ac81D0BA0dc21f33d14321b28Bd2b971",
+//   //   gas,
+//   // });
+// }
+// const  getdata = async()=>{  
+//   const data = await myContract.methods.getno().call();
+//   console.log(data.toString());
   // console.log(post)
 
 }
